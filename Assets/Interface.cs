ab2a04ec75b8e41118659b616db706d31a85b686
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2015-09-30 18:32:08 -0500

    Trying to understand
    Think I got it now

---
+++ Assets/Interface.cs
@@ -0,0 +1,16 @@
+﻿public interface ISub
+{
+    void Subcribe(string type, string msg); 
+    //function that creates a subcription that the object that inherits
+    //froms this iterface is looking for 
+    void Recive(string msg);
+    //recives an event from the observer that the subcribers has subcribed to when
+    //the event has been published from some where elese in the program
+}
+
+public interface IPub
+{
+    void Publish(string msg);
+    //The class that inherits from this interface will use this function to
+    //publish the event trigger that another object/class is subcribed to.
+}
---
+++ Assets/Observer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+//uses the singleton class created to ensure there can only be one instance of this class at any given time
+public class Observer : Singleton<Observer> 
+{
+    static Dictionary<string, ISub> Subscribers; //stores all the messages that subscribers are looking for
+
+    protected override void Awake() //Overrides the awake the unity function
+    {
+        base.Awake();
+        Subscribers = new Dictionary<string, ISub>();
+    }
+
+    //When a msg is published it will alert a subcriber that is subscribed to that msg if any
+    static public void Notify(string msg)
+    {
+        Debug.Log(msg); //displays the msg/event to the console
+        instance.NotifySubs(msg); //will notify the subscriber that is sub to this msg
+    }
+
+    private void NotifySubs(string msg)
+    {
+        foreach(KeyValuePair<string, ISub> s in Subscribers)
+        {
+            if (Subscribers.ContainsKey(msg)) //if the disctionary for subribers contains the msg
+            {
+                s.Value.Recive(msg); // the subscriber will then recive the msg
+            }
+        }
+    }
+
+    //creates the subscription from combing the sting of the type of event we are looking for and the msg
+    //Then passes the subscription and sub into the AddListener as arguments to be added to the dictionary
+    static public void AddSub(string type, string msg, ISub sub)
+    {
+        string t = type;
+        string m = msg;
+        string subcription = t + ":" + m;
+        instance.AddListener(subcription, sub);
+    }
+
+    /*
+        Will add a message and the object that uses the ISub interface to the subscriber list
+    */
+    private void AddListener(string msg, ISub sub)
+    {
+        Subscribers.Add(msg, sub);
+    }
+}
